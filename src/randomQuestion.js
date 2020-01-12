a23098d628d7e1e755b3f0d45404493991f84918
Liz <40321604+lizkavalski@users.noreply.github.com>
2020-01-11 21:06:59 -0800

    editing
    doing some editing.

--- src/randomQuestion.js
+++ src/randomQuestion.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function RandomQuestions() {
+  // let random = () => {
+  //   random[Math.floor(Math.random() * items.length)];
+  // };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-
-      <h2>Start editing to see some magic happen!</h2>
+      <button>Pick a Random Questions </button>
     </div>
   );
 }
