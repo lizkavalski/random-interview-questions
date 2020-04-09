fda8f89e2d6ed1c6797acf0fdc0f4f10cd8ff683
lizkavalski <lizkavalski@gmail.com>
2020-04-08 20:36:24 -0700

    more styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './style/style.scss';
 function App() {
   return (
     <div className="App">
+      <h1>Interview Prep!</h1>
       <p> This an example to see if this file is connected the scss file</p>
     </div>
   );
