dedc251d3ee30ef513f3701558317187a4c94f65
lizkavalski <lizkavalski@gmail.com>
2020-04-10 14:44:00 -0700

    add buttons to the wireframing

--- src/randomQuestion.js
+++ src/randomQuestion.js
@@ -2,14 +2,11 @@ import React from 'react';
 import './style/style.scss';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Interview Prep!</h1>
       <p> This an example to see if this file is connected the scss file</p>
-      <div id='buttons'>
-        <button id='before'>Previous</button> 
-        <button id= 'next'>Next</button>
-      </div>
     </div>
   );
 }
