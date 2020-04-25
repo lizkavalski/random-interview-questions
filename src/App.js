7bc6c9908268afa0b8bcf8e4bddbeb80dfd090e4
lizkavalski <lizkavalski@gmail.com>
2020-04-25 12:40:48 -0700

    little styeling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style/style.scss';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Interview Prep!</h1>
-      <p> This is where Interview questions go</p>
+      <p> This is where Interview questions go </p>
       <div id='buttons'>
         <button id='before'>Previous</button> 
         <button id= 'next'>Next</button>
