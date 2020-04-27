1e35addf261f0277c2bb9bc1ef09cc67585f2653
lizkavalski <lizkavalski@gmail.com>
2020-04-27 16:23:25 -0700

    started on the logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style/style.scss';
+import RandomQuestion from '../src/component/randomQuestion'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Interview Prep!</h1>
-      <p> This is where Interview questions go </p>
+      <RandomQuestion/>
       <div id='buttons'>
         <button id='before'>Previous</button> 
         <button id= 'next'>Next</button>
