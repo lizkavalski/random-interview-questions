de8023149136d1614e85589f3e04d00d976cb37a
lizkavalski <lizkavalski@gmail.com>
2020-05-19 14:54:34 -0700

    little bit of editing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <h1>Interview Prep!</h1>
       <RandomQuestion/>
       <div id='buttons'>
-        {/* render a new question */}
         <button id= 'next'>Next</button>
       </div>
     </div>
