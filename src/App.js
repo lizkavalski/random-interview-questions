45a2adf1a9e5d1c214b742416f2d5df370a15702
lizkavalski <lizkavalski@gmail.com>
2020-07-29 15:09:55 -0700

    can render a random question but having issuse with styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style/style.scss';
-import data from './component/data.json'
 import RandomQuestion from './component/randomQuestion';
 
 
@@ -9,9 +8,6 @@ function App() {
     <div className="App">
       <h1>Interview Prep!</h1>
       <RandomQuestion/>
-      <div id='buttons'>
-        <button id= 'next'>Next</button>
-      </div>
     </div>
   );
 }
