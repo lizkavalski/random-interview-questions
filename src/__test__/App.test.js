617c122b613a9cd6dd29ec8ab9c0f796ef782e82
lizkavalski <lizkavalski@gmail.com>
2020-12-03 21:35:14 -0800

    fixed one test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders Interview Prep', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Interview Prep/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test()
